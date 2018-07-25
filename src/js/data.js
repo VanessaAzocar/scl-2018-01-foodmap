75ede9501902b976107c3662f08855a7a2ab434e
Vanessa Azócar <v.azocar.adasme@gmail.com>
2018-07-25 12:00:43 -0400

    Mapa georeferenciado

---
+++ src/js/data.js
@@ -0,0 +1,56 @@
+let map;
+let infowindow;
+
+const initMap=() => {
+  // Creamos un mapa con las coordenadas actuales
+  navigator.geolocation.getCurrentPosition(function (pos) {
+
+    lat = pos.coords.latitude;
+    lon = pos.coords.longitude;
+
+    let myLatlng = new google.maps.LatLng(lat, lon);
+
+    let mapOptions = {
+      center: myLatlng,
+      zoom: 14,
+      mapTypeId: google.maps.MapTypeId.MAP
+    };
+
+    map = new google.maps.Map(document.getElementById("mapa"), mapOptions);
+
+    // Creamos el infowindow
+    infowindow = new google.maps.InfoWindow();
+
+    // Especificamos la localización, el radio y el tipo de lugares que queremos obtener
+    let request = {
+      location: myLatlng,
+      radius: 5000,
+      types: ['cafe', 'restaurante']
+    };
+
+    // Creamos el servicio PlaceService y enviamos la petición.
+    let service = new google.maps.places.PlacesService(map);
+
+    service.nearbySearch(request, function (results, status) {
+      if (status === google.maps.places.PlacesServiceStatus.OK) {
+        for (let i = 0; i < results.length; i++) {
+          crearMarcador(results[i]);
+        }
+      }
+    });
+  });
+}
+
+const crearMarcador = (place) => {
+  // Creamos un marcador
+  let marker = new google.maps.Marker({
+    map: map,
+    position: place.geometry.location
+  });
+
+  // Asignamos el evento click del marcador
+  google.maps.event.addListener(marker, 'click', function () {
+    infowindow.setContent(place.name);
+    infowindow.open(map, this);
+  });
+}
