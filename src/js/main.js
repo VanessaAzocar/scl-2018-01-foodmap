c1f70a3669a48a48befcc547596a9f8df455ac0d
Vanessa Azócar <v.azocar.adasme@gmail.com>
2018-07-26 12:54:49 -0400

    mapa arreglado

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,8 @@
 window.onload = (() => {
- /* Para esconder la pantalla de inicio*/
+  /* Para esconder la pantalla de inicio*/
   setTimeout(function hide() { $('#pagSplash').hide('fast');
-  document.getElementById('pagApp').style.display = 'block'; }, 3000);
-  
-  /*para que aparezca el mapa */
+    document.getElementById('pagApp').style.display = 'block'; }, 3000);
+  /* para que aparezca el mapa*/
   initMap();
-
-});// fin de window onload
+});
 
